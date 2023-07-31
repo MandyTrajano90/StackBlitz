056d77bbf1d2ffaead04f2ac2a4d08681a1871f7
MandyTrajano90 <131377483+MandyTrajano90@users.noreply.github.com>
2023-07-31 15:32:33 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import './style.css';
+
+import {
+  fillUsersSelect,
+  fillPosts,
+  fillFeaturedPostComments,
+  clearPageData,
+  fillErrorMessage,
+} from './utils/updateUI';
+
+const usersSelect = document.querySelector('#users-select');
+
+const USERS_API = 'https://dummyjson.com/users';
+fetch(USERS_API)
+  .then((response) => response.json())
+  .then((data) => {
+    const { users } = data;
+    fillUsersSelect(users);
+  });
+
+usersSelect.addEventListener('change', () => {
+  clearPageData();
+  const POST_API = `https://dummyjson.com/posts/user/${usersSelect.value}`;
+
+  fetch(POST_API)
+    .then((response) => response.json())
+    .then((data) => {
+      const { post } = data;
+      fillPosts(post);
+    });
+});
